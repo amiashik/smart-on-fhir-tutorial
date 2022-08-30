458f03576998893ce4637a5471810c2faae435ed
amiashik <112124160+amiashik@users.noreply.github.com>
2022-08-30 14:16:29 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -17,7 +17,7 @@
                       code: {
                         $or: ['http://loinc.org|8302-2', 'http://loinc.org|85354-9',
                               'http://loinc.org|2085-9', 'http://loinc.org|2089-1', 
-                              'http://loinc.org|55284-4']
+                              'http://loinc.org|55284-4', 'http://loinc.org|8310-5']
                       }
                     }
                   });
@@ -41,6 +41,7 @@
           var diastolicbp = getBloodPressureValue(byCodes('85354-9'),'8462-4');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
+          var height = byCodes('8310-5');
 
           var p = defaultPatient();
           p.birthdate = patient.birthDate;
